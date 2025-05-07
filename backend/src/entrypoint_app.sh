561c3b265e706f54d636f1428d83335ee4dc582b
ya_novikov <arhdomain@gmail.com>
2025-05-07 16:48:02 +0300

    add .gitlab-ci.yml

--- backend/src/entrypoint_app.sh
+++ backend/src/entrypoint_app.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+set -e
+
+echo "Running migrations..."
 alembic upgrade head
+echo "Migrations complete"
+
+echo "Starting application..."
 uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2
